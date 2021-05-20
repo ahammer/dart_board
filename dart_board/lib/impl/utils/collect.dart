f1543c6978e5dea0089df3cdca7324758e4fcb2c
Adam Hammer <adamhammer2@gmail.com>
2021-05-19 22:02:05 -0700

    Adding pedantic all around

--- dart_board/lib/impl/utils/collect.dart
+++ dart_board/lib/impl/utils/collect.dart
@@ -1,6 +1,7 @@
 /// To Collect a map in a list through a reducer.
 extension MapCollect<K, V, R> on Map<K, V> {
-  List<R> collect(R Function(K key, V value) convert) {
+  // ignore: avoid_shadowing_type_parameters
+  List<R> collect<R>(R Function(K key, V value) convert) {
     final result = <R>[];
     forEach((key, value) => result.add(convert(key, value)));
     return result;
