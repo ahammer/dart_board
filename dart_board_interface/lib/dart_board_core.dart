66cd33261a819788c51080e1290a8a5021eeabcb
Adam Hammer <adamhammer@gmail.com>
2021-05-21 20:44:50 -0700

    Documentation

--- dart_board_interface/lib/dart_board_core.dart
+++ dart_board_interface/lib/dart_board_core.dart
@@ -5,25 +5,45 @@ import 'package:provider/provider.dart';
 /// Communicates with Core. For features
 GlobalKey<NavigatorState> dartBoardNavKey = GlobalKey();
 
-/// Shortcuts into DartBoard
+/// Dart Board Core
+///
+/// This is the basic interface into DartBoard
+///
+/// It stores the Features,
 abstract class DartBoardCore {
+  /// These are the Features
+  ///
+  /// The implementation is responsible for managing this list
   List<DartBoardFeature> get features;
-  List<RouteDefinition> get routes;
 
-  /// Apply the page decorations manually to a widget
+  /// These are the RouteDefinitions
+  ///
+  /// They are exposed for debug/info purposes primarily
+  List<RouteDefinition> get routes => features.fold(
+      [], (previousValue, element) => [...previousValue, ...element.routes]);
+
+  /// Apply Page Decorations to a widget.
+  ///
+  /// child:
+  ///
+  /// E.g. if you use RouteWidget with like a dialog, but want the decorations
   Widget applyPageDecorations(Widget child);
 
-  /// Build a page route based on a RouteDefinition and Settings
+  /// Builds a page route
+  ///
+  /// This is in the interface
   Widget buildPageRoute(
       BuildContext context, RouteSettings settings, RouteDefinition definition);
 
-  /// Helper to decorate a page
+  ///------------------------------------------------------------------------------------------------------------
+  /// STATIC HELPERS
+
   static Widget decoratePage(Widget child) =>
       Provider.of<DartBoardCore>(dartBoardNavKey.currentContext!)
           .applyPageDecorations(child);
 
   /// Helper to get a list of the features
-  static List<DartBoardFeature> getfeatures() =>
+  static List<DartBoardFeature> get featureList =>
       Provider.of<DartBoardCore>(dartBoardNavKey.currentContext!).features;
 
   /// Helper to find an feature by name
