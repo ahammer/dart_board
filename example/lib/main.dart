cef634601bda9e62b65c3119391f9075f3014306
Adam Hammer <adamhammer@gmail.com>
2021-05-25 20:23:18 -0700

    improving documentation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,3 +12,23 @@ void main() => runApp(DartBoard(
       /// And set your entry point
       initialRoute: '/main',
     ));
+
+/// ----------------------------------------------
+/// 
+/// What is happening here?
+/// 
+/// When you start the App, you specify the features
+/// you'd like to support. In this case I'm bringing the 
+/// Example Feature in.
+/// 
+/// The example feature includes more Features, and offers
+/// integration examples.
+/// 
+/// DartBoard will collect all these Features and collect
+/// their capabilities (Routes/Decorations).
+/// 
+/// It will then inflate your initial route as your home page 
+/// and initialize the framework to help decorate your pages
+/// and install any app-level state/decorations from the features.
+/// 
+
