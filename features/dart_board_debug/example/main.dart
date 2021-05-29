5709f85311cfaa9e1542e2fa30e2a3c81d32fd9b
Adam Hammer <adamhammer@gmail.com>
2021-05-29 14:39:43 -0700

    clean up

--- features/dart_board_debug/example/main.dart
+++ features/dart_board_debug/example/main.dart
@@ -2,12 +2,6 @@ import 'package:dart_board_core/dart_board.dart';
 import 'package:dart_board_debug/debug_feature.dart';
 import 'package:flutter/cupertino.dart';
 
-/// Just loading the extansion and showing the route
-///
-/// It's better to work on this while connected to an integrated app right now
-/// (saves a ton of time)
-///
-/// However, in the future we can provide mocks/etc to show the AB/Disable
-/// features
+/// Just loading the feature and showing the route
 void main() =>
     runApp(DartBoard(initialRoute: '/debug', features: [DebugFeature()]));
