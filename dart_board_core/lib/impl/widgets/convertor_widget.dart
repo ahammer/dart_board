c28ee3c8e27b76d9e9176e98cf98c37c585a33a3
Adam Hammer <adamhammer@gmail.com>
2021-06-13 00:38:02 -0700

    optimize convertor

--- dart_board_core/lib/impl/widgets/convertor_widget.dart
+++ dart_board_core/lib/impl/widgets/convertor_widget.dart
@@ -1,8 +1,10 @@
 import 'package:dart_board_core/dart_board.dart';
 
 /// Converts a Builder from One type into Another
+/// Caches values and doesn't rebuild unless model or conversion
+/// changes
 /// E.g. To generate a View Model
-class BuilderConvertor<In, Out> extends StatelessWidget {
+class BuilderConvertor<In, Out> extends StatefulWidget {
   final In input;
   final Out Function(In input) convertor;
   final Widget Function(BuildContext context, Out model) builder;
@@ -15,5 +17,35 @@ class BuilderConvertor<In, Out> extends StatelessWidget {
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) => builder(context, convertor(input));
+  _BuilderConvertorState<In, Out> createState() =>
+      _BuilderConvertorState<In, Out>();
+}
+
+class _BuilderConvertorState<In, Out> extends State<BuilderConvertor<In, Out>> {
+  late In _current;
+  late Out _value;
+
+  @override
+  void didUpdateWidget(covariant BuilderConvertor<In, Out> oldWidget) {
+    if (_current == widget.input) return;
+    final _newValue = widget.convertor(widget.input);
+    if (_newValue == _value) return;
+
+    setState(() {
+      _current = widget.input;
+      _value = widget.convertor(_current);
+    });
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void initState() {
+    _current = widget.input;
+    _value = widget.convertor(_current);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(context, _value);
 }
