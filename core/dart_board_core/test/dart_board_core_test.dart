8ab7daeb0cb3451c3a1710642cc1670f5603bedc
Adam Hammer <adamhammer2@gmail.com>
2021-06-19 18:24:22 -0700

    fix: adding tests and coverage

--- core/dart_board_core/test/dart_board_core_test.dart
+++ core/dart_board_core/test/dart_board_core_test.dart
@@ -2,10 +2,53 @@ import 'package:dart_board_core/dart_board.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Blank Run Test', (tester) async {
+  testWidgets('404 With no Features', (tester) async {
     await tester.pumpWidget(DartBoard(
       initialRoute: '/main',
       features: [],
     ));
+
+    // Make sure DartBoard as started fully
+    await tester.pumpAndSettle();
+
+    // Nothing is registered, so this should be 404 message
+    // Checking for default
+    expect(find.text('"/main" Not Found'), findsOneWidget);
+  });
+
+  testWidgets('Basic Entry Point Loading', (tester) async {
+    await tester.pumpWidget(DartBoard(
+      initialRoute: '/main',
+      features: [TestFeature()],
+    ));
+
+    // Make sure DartBoard as started fully
+    await tester.pumpAndSettle();
+
+    // Nothing is registered, so this should be 404 message
+    // Checking for default
+    expect(find.text('main displayed'), findsOneWidget);
   });
 }
+
+class TestFeature extends DartBoardFeature {
+  @override
+  String get namespace => 'test feature';
+
+  @override
+  List<RouteDefinition> get routes => [
+        NamedRouteDefinition(
+            route: '/main',
+            builder: (settings, ctx) =>
+                Container(child: Text('main displayed')))
+      ];
+}
+
+DartBoardDecoration getTestDecoration(String label) => DartBoardDecoration(
+    decoration: (ctx, child) => Column(
+          children: [
+            Text(label),
+            child,
+          ],
+        ),
+    name: 'tester');
