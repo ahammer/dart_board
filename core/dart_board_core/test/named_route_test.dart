0c349c65689a52893799ee9cdda4ad3c4849cf0b
Adam Hammer <adamhammer@gmail.com>
2021-07-11 15:13:39 -0700

    better testing for name route

---
+++ core/dart_board_core/test/named_route_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_board_core/dart_board.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Test the NamedRoute class', (tester) async {
+    final RouteDefinition def = NamedRouteDefinition(
+        route: 'test',
+        builder: (settings, ctx) =>
+            MaterialApp(home: Material(child: Text('visible'))));
+    await tester.pumpWidget(Builder(
+        builder: (ctx) => def.builder(RouteSettings(name: 'test'), ctx)));
+    await tester.pumpAndSettle();
+    expect(def.toString(), equals('test'));
+    expect(find.text('visible'), findsOneWidget);
+  });
+}
