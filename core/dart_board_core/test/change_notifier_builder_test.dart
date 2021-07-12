1c9fdfd2bd4da356983f820a01667abf411ffdec
Adam Hammer <adamhammer@gmail.com>
2021-07-11 22:11:02 -0700

    tests: crossed off some coverage chunks

--- core/dart_board_core/test/change_notifier_builder_test.dart
+++ core/dart_board_core/test/change_notifier_builder_test.dart
@@ -6,12 +6,25 @@ void main() {
   // Verifies the Change Notifier is listening
   testWidgets('test ChangeNotifierBuilder widget', (tester) async {
     final notifier = MyState();
-    await tester.pumpWidget(MaterialApp(home:
-       ChangeNotifierBuilder<MyState>(
+    await tester.pumpWidget(MaterialApp(
+      home: ChangeNotifierBuilder<MyState>(
         notifier: notifier,
         builder: (ctx, val) => Text(val.output),
       ),
     ));
+    await tester.pumpAndSettle();
+    expect(find.text('0'), findsOneWidget);
+    notifier.increment();
+    await tester.pumpAndSettle();
+    expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('test ChangeNotifierBuilder widget - Extension Syntax',
+      (tester) async {
+    final notifier = MyState();
+    await tester.pumpWidget(MaterialApp(
+        home:
+            notifier.builder<MyState>((context, value) => Text(value.output))));
     await tester.pumpAndSettle();
     expect(find.text('0'), findsOneWidget);
     notifier.increment();
--- core/dart_board_core/test/dart_board_core_test.dart
+++ core/dart_board_core/test/dart_board_core_test.dart
@@ -141,6 +141,7 @@ void main() {
         DartBoardCore.instance.findByName('namespace_not_found');
 
     expect(emptyFeature is EmptyDartBoardFeature, equals(true));
+    expect(emptyFeature.toString(), equals('EmptyDartBoardFeature'));
   });
 
   testWidgets('Check page navigation to a 404', (tester) async {
