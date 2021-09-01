b6a718ab67f59020492d627d62a00e874b386ffb
Adam Hammer <adamhammer@gmail.com>
2021-08-31 23:22:27 -0700

    fix: mobile support for particle/cursor features. Filtering out big jumps for better UI with it too.

--- features/dart_board_particles/lib/features/cursor_particle_features.dart
+++ features/dart_board_particles/lib/features/cursor_particle_features.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:dart_board_core/dart_board.dart';
 import 'package:dart_board_particles/presets/fire_particle.dart';
 import 'package:dart_board_particles/dart_board_particle_feature.dart';
 import 'package:dart_board_particles/presets/rainbow_particle.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 abstract class CursorFeature extends DartBoardFeature {
@@ -12,19 +15,43 @@ abstract class CursorFeature extends DartBoardFeature {
 
   @override
   List<DartBoardDecoration> get pageDecorations => [
-        DartBoardDecoration(
-            decoration: (BuildContext context, Widget child) => MouseRegion(
-                  key: ValueKey('${namespace}_mouse_region'),
-                  onHover: (hover) {
-                    Particles.instance.addLayer(generateParticleLayer(
-                        hover.position.dx, hover.position.dy));
-                  },
-                  child: child,
-                ),
-            name: '${namespace}_overlay'),
+        /// For Mouse-enabled platforms we'll use MouseRegion
+        if (kIsWeb ||
+            Platform.isLinux ||
+            Platform.isWindows ||
+            Platform.isMacOS)
+          DartBoardDecoration(
+              decoration: (BuildContext context, Widget child) => MouseRegion(
+                    key: ValueKey('${namespace}_mouse_region'),
+                    onHover: (hover) {
+                      if (hover.position.dx != 0 && hover.position.dy != 0)
+                        return Particles.instance.addLayer(
+                            generateParticleLayer(
+                                hover.position.dx, hover.position.dy));
+                    },
+                    child: child,
+                  ),
+              name: '${namespace}_overlay')
+        else
+          DartBoardDecoration(
+              decoration: (BuildContext context, Widget child) =>
+                  GestureDetector(
+                    excludeFromSemantics: true,
+                    key: ValueKey('${namespace}_mouse_region'),
+                    onPanUpdate: (hover) {
+                      if (hover.globalPosition.dx != 0 &&
+                          hover.globalPosition.dy != 0)
+                        return Particles.instance.addLayer(
+                            generateParticleLayer(hover.globalPosition.dx,
+                                hover.globalPosition.dy));
+                    },
+                    child: child,
+                  ),
+              name: '${namespace}_overlay'),
       ];
 }
 
+/// Add a flame to your cursor
 class FireCursorFeature extends CursorFeature {
   @override
   String get namespace => 'FireCursor';
@@ -33,6 +60,7 @@ class FireCursorFeature extends CursorFeature {
       FireParticleLayer(x, y);
 }
 
+/// Add a rainbow to your cursor
 class RainbowCursorFeature extends CursorFeature {
   double? oldY;
   double? oldX;
