bf9adc430c1efdb68e4d0bfe1a38e4b6bcf0f5fa
Adam Hammer <adamhammer2@gmail.com>
2021-09-27 23:34:00 -0700

    cubit and bloc examples

--- features/dart_board_bloc/lib/main.dart
+++ features/dart_board_bloc/lib/main.dart
@@ -23,6 +23,27 @@ class CounterCubit extends Cubit<int> {
   void decrement() => emit(state - 1);
 }
 
+/// The events which `CounterBloc` will react to.
+abstract class CounterEvent {}
+
+/// Notifies bloc to increment state.
+class Increment extends CounterEvent {
+  final int amount;
+
+  Increment(this.amount);
+}
+
+/// A `CounterBloc` which handles converting `CounterEvent`s into `int`s.
+class CounterBloc extends Bloc<CounterEvent, int> {
+  /// The initial state of the `CounterBloc` is 0.
+  CounterBloc() : super(0) {
+    /// When an `Increment` event is added,
+    /// the current `state` of the bloc is accessed via the `state` property
+    /// and a new state is emitted via `emit`.
+    on<Increment>((event, emit) => emit(state + event.amount));
+  }
+}
+
 /// This feature exposes a route and a Cubit
 class CubitExampleFeature extends DartBoardFeature {
   @override
@@ -36,8 +57,10 @@ class CubitExampleFeature extends DartBoardFeature {
       ];
 
   @override
-  List<DartBoardDecoration> get appDecorations =>
-      [BlocDecoration((ctx) => CounterCubit())];
+  List<DartBoardDecoration> get appDecorations => [
+        CubitDecoration((ctx) => CounterCubit()),
+        BlocDecoration((ctx) => CounterBloc()),
+      ];
 }
 
 /// Cubit Page that shows a counter
@@ -46,20 +69,41 @@ class CubitPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: BlocBuilder<CounterCubit, int>(
-          builder: (ctx, state) => Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('count $state'),
-                MaterialButton(
-                    onPressed: () => ctx.read<CounterCubit>().increment(),
-                    child: const Text('Increment')),
-                MaterialButton(
-                    onPressed: () => ctx.read<CounterCubit>().decrement(),
-                    child: const Text('Decrement')),
-              ],
-            ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              BlocBuilder<CounterCubit, int>(
+                builder: (ctx, state) => Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+
+                    Text('Cubit:  $state'),
+                    MaterialButton(
+                        onPressed: () => ctx.read<CounterCubit>().increment(),
+                        child: const Text('Increment')),
+                    MaterialButton(
+                        onPressed: () => ctx.read<CounterCubit>().decrement(),
+                        child: const Text('Decrement')),
+                  ],
+                ),
+              ),
+               BlocBuilder<CounterBloc, int>(
+                builder: (ctx, state) => Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+
+                    Text('Bloc:  $state'),
+                    MaterialButton(
+                        onPressed: () => ctx.read<CounterBloc>().add(Increment(1)),
+                        child: const Text('Increment')),
+                    MaterialButton(
+                        onPressed: () => ctx.read<CounterBloc>().add(Increment(-1)),
+                        child: const Text('Decrement')),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       );
