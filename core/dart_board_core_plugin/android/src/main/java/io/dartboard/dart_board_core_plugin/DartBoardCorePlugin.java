170410b7c24cfe4515788232c88a52a7d14f8ddb
Adam Hammer <adamhammer@gmail.com>
2021-10-02 21:13:18 -0700

    pigeons and Plugin Binding

--- core/dart_board_core_plugin/android/src/main/java/io/dartboard/dart_board_core_plugin/DartBoardCorePlugin.java
+++ core/dart_board_core_plugin/android/src/main/java/io/dartboard/dart_board_core_plugin/DartBoardCorePlugin.java
@@ -2,6 +2,7 @@ package io.dartboard.dart_board_core_plugin;
 
 import androidx.annotation.NonNull;
 
+import io.dartboard.navapi.Api;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -9,30 +10,21 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** DartBoardCorePlugin */
-public class DartBoardCorePlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+public class DartBoardCorePlugin implements FlutterPlugin {
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "dart_board_core_plugin");
-    channel.setMethodCallHandler(this);
+  /// getter for safety
+  public static Api.Nav getNav() {
+    return _nav;
   }
 
+  private static Api.Nav _nav;
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    _nav = new Api.Nav(flutterPluginBinding.getBinaryMessenger());
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+    // Do nothing
   }
 }
--- core/dart_board_core_plugin/lib/dart_board_core_plugin.dart
+++ core/dart_board_core_plugin/lib/dart_board_core_plugin.dart
@@ -1,13 +1,25 @@
-
 import 'dart:async';
 
-import 'package:flutter/services.dart';
+import 'package:dart_board_core/dart_board.dart';
+import 'package:dart_board_core_plugin/nav_api.dart';
+import 'package:flutter/material.dart';
 
-class DartBoardCorePlugin {
-  static const MethodChannel _channel = MethodChannel('dart_board_core_plugin');
+class DartBoardAdd2AppFeatures extends DartBoardFeature {
+  @override
+  String get namespace => "Add2App";
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  @override
+  List<DartBoardDecoration> get appDecorations => [
+        DartBoardDecoration(
+            name: 'Add2App',
+            decoration: (ctx, child) => LifeCycleWidget(
+                preInit: () => Nav.setup(NavApi()),
+                key: const ValueKey("Add2App_LC"),
+                child: child))
+      ];
+}
+
+class NavApi extends Nav {
+  @override
+  void setNavRoot(String route) => DartBoardCore.nav.replaceRoot(route);
 }
