849ac2f75f0385e6410a6a0fffaca473188fd7b8
Adam Hammer <adamhammer@gmail.com>
2021-10-04 00:24:04 -0700

    fix: made the plugin work in example

--- features/dart_board_spacex_feature/dart_board_spacex_plugin/example/android/app/src/main/java/io/dartboard/dart_board_spacex_plugin_example/MainActivity.java
+++ features/dart_board_spacex_feature/dart_board_spacex_plugin/example/android/app/src/main/java/io/dartboard/dart_board_spacex_plugin_example/MainActivity.java
@@ -15,6 +15,11 @@ public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         DartBoardSpacexPlugin.getApi().getLaunches(reply -> {
             Toast.makeText(this, "Found Results"+reply.size(), Toast.LENGTH_LONG).show();
         });
