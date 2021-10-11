e8a1185edad66d011e391f9195097c21bf5aee78
Adam Hammer <adamhammer@gmail.com>
2021-10-10 20:32:04 -0700

    feat: more caching in the repository (launches/launch/rocket)

--- features/dart_board_spacex_feature/dart_board_spacex_repository/lib/impl/spacex_repository_graphql.dart
+++ features/dart_board_spacex_feature/dart_board_spacex_repository/lib/impl/spacex_repository_graphql.dart
@@ -9,6 +9,8 @@ class SpaceXRepositoryGraphQL extends SpaceXRepository {
   final _client = ArtemisClient(_ENDPOINT);
 
   List<GetPastLaunches$Query$Launch?>? _pastLaunches;
+  List<GetRockets$Query$Rocket?>? _rockets;
+  Map<String, GetLaunch$Query$Launch?> _launches = {};
 
   @override
   Future<List<GetPastLaunches$Query$Launch?>> getPastLaunches() async {
@@ -19,14 +21,22 @@ class SpaceXRepositoryGraphQL extends SpaceXRepository {
   }
 
   @override
-  Future<GetLaunch$Query$Launch?> getLaunchByMissionId(
-          String missionId) async =>
-      (await _client.execute(
-              GetLaunchQuery(variables: GetLaunchArguments(id: missionId))))
-          .data
-          ?.launch;
+  Future<GetLaunch$Query$Launch?> getLaunchByMissionId(String missionId) async {
+    if (_launches.containsKey(missionId)) {
+      return _launches[missionId]!;
+    }
+
+    _launches[missionId] = (await _client.execute(
+            GetLaunchQuery(variables: GetLaunchArguments(id: missionId))))
+        .data
+        ?.launch;
+    return _launches[missionId]!;
+  }
 
   @override
-  Future<List<GetRockets$Query$Rocket?>> getRockets() async =>
-      (await _client.execute(GetRocketsQuery())).data!.rockets!;
+  Future<List<GetRockets$Query$Rocket?>> getRockets() async {
+    if (_rockets != null) return _rockets!;
+    _rockets = (await _client.execute(GetRocketsQuery())).data!.rockets!;
+    return _rockets!;
+  }
 }
