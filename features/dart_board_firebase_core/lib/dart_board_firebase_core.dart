53326dbfd8ff4747d6d4cde545edd75d8ac57ebd
Adam Hammer <adamhammer2@gmail.com>
2025-03-10 01:01:51 -0700

    fix up firebase init

--- features/dart_board_firebase_core/lib/dart_board_firebase_core.dart
+++ features/dart_board_firebase_core/lib/dart_board_firebase_core.dart
@@ -1,19 +1,30 @@
 import 'dart:io';
-import 'dart:js' as js;
-
 import 'package:dart_board_core/dart_board_core.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:js_interop';
 
-/// Initialization Hook for Firebase App
-/// For firebase-capable features to use
+@JS('firebaseConfig')
+external JSObject? get firebaseConfig;
+
+extension FirebaseConfigExtension on JSObject {
+  external String get apiKey;
+  external String get appId;
+  external String get messagingSenderId;
+  external String get projectId;
+  external String? get authDomain;
+  external String? get storageBucket;
+  external String? get measurementId;
+}
+
 class DartBoardFirebaseCoreFeature extends DartBoardFeature {
   @override
   List<DartBoardDecoration> get appDecorations => [
         DartBoardDecoration(
-            name: "FirebaseApp",
-            decoration: (ctx, child) => FirebaseGateway(child: child))
+          name: "FirebaseApp",
+          decoration: (ctx, child) => FirebaseGateway(child: child),
+        )
       ];
 
   @override
@@ -24,19 +35,18 @@ class DartBoardFirebaseCoreFeature extends DartBoardFeature {
       kIsWeb || Platform.isAndroid || Platform.isIOS || Platform.isMacOS;
 }
 
-// Blocks the app from starting before firebase init'd
 class FirebaseGateway extends StatefulWidget {
   final Widget child;
 
   FirebaseGateway({required this.child})
-      : super(key: ValueKey("FirebaseGateway"));
+      : super(key: const ValueKey("FirebaseGateway"));
 
   @override
   _FirebaseGatewayState createState() => _FirebaseGatewayState();
 }
 
 class _FirebaseGatewayState extends State<FirebaseGateway> {
-  late final initFuture;
+  late final Future<FirebaseApp> initFuture;
 
   @override
   void initState() {
@@ -46,35 +56,33 @@ class _FirebaseGatewayState extends State<FirebaseGateway> {
 
   Future<FirebaseApp> _initializeFirebase() async {
     FirebaseOptions? options;
-    
-    if (kIsWeb) {
-      // Check if firebaseConfig is available in the window object
-      if (js.context.hasProperty('firebaseConfig')) {
-        final config = js.JsObject.fromBrowserObject(js.context['firebaseConfig']);
-        options = FirebaseOptions(
-          apiKey: config['apiKey'],
-          appId: config['appId'],
-          messagingSenderId: config['messagingSenderId'],
-          projectId: config['projectId'],
-          authDomain: config['authDomain'],
-          storageBucket: config['storageBucket'],
-          measurementId: config['measurementId'],
-        );
-      }
+
+    if (kIsWeb && firebaseConfig != null) {
+      final config = firebaseConfig!;
+      options = FirebaseOptions(
+        apiKey: config.apiKey,
+        appId: config.appId,
+        messagingSenderId: config.messagingSenderId,
+        projectId: config.projectId,
+        authDomain: config.authDomain,
+        storageBucket: config.storageBucket,
+        measurementId: config.measurementId,
+      );
     }
-    
+
     return await Firebase.initializeApp(options: options);
   }
 
   @override
-  Widget build(BuildContext context) => FutureBuilder(
-      future: initFuture,
-      builder: (ctx, snapshot) {
-        if (snapshot.hasError) {
-          return Container(child: Text('${snapshot.error}'));
-        }
-        return snapshot.connectionState == ConnectionState.done
-            ? widget.child
-            : Container();
-      });
+  Widget build(BuildContext context) => FutureBuilder<FirebaseApp>(
+        future: initFuture,
+        builder: (ctx, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          return snapshot.connectionState == ConnectionState.done
+              ? widget.child
+              : const Center(child: CircularProgressIndicator());
+        },
+      );
 }
